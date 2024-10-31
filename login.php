5c8e085dc4d2a8449a30c82b988c80f32fcc1290
rizqiaulia07 <riskisunar2@gmail.com>
2024-10-31 14:04:05 +0700

    upload pertama
    di upload di web

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+<div class="container">
+      
+      <!-- Outer Row -->
+      <div class="row justify-content-center">
+  
+          <div class="col-xl-10 col-lg-12 col-md-9">
+  
+              <div class="card o-hidden border-0 shadow-lg my-5">
+                  <div class="card-body p-0">
+                      <!-- Nested Row within Card Body --> 
+                      <div class="p-5">
+                      <div class="text-center">
+                                     <b><h1 class="h4 text-gray-900 mb-4">SEKOLAH DASAR NEGERI 1</b></h1>
+                                  </div>
+                      <div class="row">
+                          <div class="container">
+                         <div class="d-flex justify-content-center" style="margin-top:24px">
+                          <img src="img/LOGO.png" width="250px" class="img-fuid" alt="responsive image" class="resize">
+                         </div>
+                        
+                          <div class="p-5">
+                                  <div class="text-center">
+                                   
+                              
+                                      
+                                      <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                          Login
+                                      </a>
+                                
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+  
+          </div>
+  
+      </div>
+  
+  </div>
+</section>
